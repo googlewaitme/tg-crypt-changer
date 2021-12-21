8ed4f595e7923924bc634d711f33312689df273f
Bulat <kusok.apelbsina@gmail.com>
2021-12-21 16:34:44 +0300

    Добавил сохранение значения монеты на 18 минут

--- utils/coin_api/api.py
+++ utils/coin_api/api.py
@@ -1,14 +1,22 @@
 from coinbase.wallet.client import Client
+from datetime import datetime, timedelta
 
 
 class CoinbaseApi:
     def __init__(self, api_key, api_secret):
         self.client = Client(api_key, api_secret)
+        self.bufer = dict()
+        self.bufering_time = timedelta(minutes=18)
 
     def get_coin_price(self, base='BTC', currency='RUB'):
-        # TODO saving of price for 5 minutes
-        currency_pair = f"{base}-{currency}"
-        return self.client.get_buy_price(currency_pair=currency_pair)
+        request = f"{base}-{currency}"
+        if request in self.bufer:
+            delta_time = datetime.now() - self.bufer[request]['time']
+            if delta_time <= self.bufering_time:
+                return self.bufer[request]['price']
+        price = self.client.get_buy_price(currency_pair=request)['amount']
+        self.bufer[request] = {'price': price, 'time': datetime.now()}
+        return price
 
     def get_current_user(self):
         return self.client.get_current_user()
