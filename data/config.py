acce79143218a0e4c1a3e39dfe41c44441e90687
Bulat <kusok.apelbsina@gmail.com>
2022-01-05 16:36:54 +0300

    Добавил дополнительные переменные в config-file

--- data/config.py
+++ data/config.py
@@ -4,10 +4,25 @@ from environs import Env
 env = Env()
 env.read_env()
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~
+# ~~~ Главные штуки    ~~~
+# ~~~~~~~~~~~~~~~~~~~~~~~~
 BOT_TOKEN = env.str("BOT_TOKEN")  # Забираем значение типа str
 ADMINS = env.list("ADMINS")  # Тут у нас будет список из админов
 IP = env.str("IP")  # Тоже str, но для айпи адреса хоста
 COINBASE_API_SECRET = env.str('COINBASE_API_SECRET')
 COINBASE_API_KEY = env.str('COINBASE_API_KEY')
+# ~~~~~~~~~~~~~~~~~~~~~~~~
+# ~~~ Доп.штуки        ~~~
+# ~~~~~~~~~~~~~~~~~~~~~~~~
+URL_TO_RULES = env.str('URL_TO_RULES')
+BOSS_ID = env.str('BOSS_ID')
+URL_TO_BOSS = env.str('URL_TO_BOSS')
+OPERATOR_ID = env.str('OPERATOR_ID')
+URL_TO_OPERATOR = env.str('URL_TO_OPERATOR')
+CARD_NUMBER = env.str('CARD_NUMBER')
+# ~~~~~~~~~~~~~~~~~~~~~~~~
+# ~~~ Массивчики       ~~~
+# ~~~~~~~~~~~~~~~~~~~~~~~~
 
 CURRENCYES = ['BTC', "LTC"]
