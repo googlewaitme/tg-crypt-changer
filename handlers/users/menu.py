4257eb29a952b5f01a8f91cfaacbeffd8778c67a
Bulat <kusok.apelbsina@gmail.com>
2022-01-05 16:38:50 +0300

    Доработал функцию menu, чтобы очищался FSM

--- handlers/users/menu.py
+++ handlers/users/menu.py
@@ -1,5 +1,6 @@
 from aiogram import types
 from aiogram.dispatcher.filters import Text
+from aiogram.dispatcher import FSMContext
 
 from loader import dp
 from data import messages
@@ -7,7 +8,9 @@ from keyboards.default import menu_key
 
 
 @dp.message_handler(Text(["/menu", 'меню'], ignore_case=True), state='*')
-async def send_menu(message: types.Message):
+async def send_menu(message: types.Message, state: FSMContext):
+    if state:
+        await state.finish()
     keyboard = menu_key.get_markup()
     text = messages.MENU_MESSAGE
     await message.answer(text, reply_markup=keyboard)
