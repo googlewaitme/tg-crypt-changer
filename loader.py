d4e6dfc57ae491b30b4b4aa94c3394e1e4acad54
Bulat <kusok.apelbsina@gmail.com>
2022-02-20 12:25:18 +0300

    chore/8: Подключение логирования

--- loader.py
+++ loader.py
@@ -1,9 +1,13 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
+import logging
+
 from data import config
 from utils.coin_api import CoinbaseApi
 
+
+logging.basicConfig(level=logging.DEBUG)
 bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
--- middlewares/__init__.py
+++ middlewares/__init__.py
@@ -1,11 +1,11 @@
-from aiogram import Dispatcher
-
 from loader import dp
+
 from .register_user import RegisterUser
 from .user_is_baned import UserIsBaned
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
 
 
 if __name__ == "middlewares":
     dp.middleware.setup(RegisterUser())
     dp.middleware.setup(UserIsBaned())
-
+    dp.middleware.setup(LoggingMiddleware())
