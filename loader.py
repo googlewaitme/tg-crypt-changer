50be75037c9bbbddf8089f493b9691f77bd5f250
Bulat <kusok.apelbsina@gmail.com>
2022-02-22 14:28:40 +0300

    feature/18: added some currencyes in loader

--- loader.py
+++ loader.py
@@ -3,6 +3,7 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 import logging
 
+from utils.coin_api.currencyes import BitcoinCurrency, LitcoinCurrency
 from data import config
 from utils.coin_api import CoinbaseApi
 
@@ -12,3 +13,7 @@ bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 coin_api = CoinbaseApi(config.COINBASE_API_KEY, config.COINBASE_API_SECRET)
+currencyes = [
+    BitcoinCurrency(coin_api),
+    LitcoinCurrency(coin_api),
+]
