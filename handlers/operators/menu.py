c93e5145b321afbeb095ec4cafc54456c47cf7aa
Bulat <kusok.apelbsina@gmail.com>
2022-02-22 14:27:53 +0300

    bugfix/17: changed function menu for operators

--- handlers/operators/menu.py
+++ handlers/operators/menu.py
@@ -1,9 +1,12 @@
 from loader import dp
 from aiogram import types
+from aiogram.dispatcher.filters import Text
+from aiogram.dispatcher import FSMContext
 from keyboards.default import operator_menu_key
 
 
-@dp.message_handler(is_operator=True)
-async def send_menu(message: types.Message):
+@dp.message_handler(Text('Меню'), is_operator=True, state='*')
+async def send_menu(message: types.Message, state: FSMContext):
     markup = operator_menu_key.get_markup()
     await message.answer('Меню', reply_markup=markup)
+    await state.finish()
